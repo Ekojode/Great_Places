5752959890b6c458e2b04151c08168c5ab6f733d
Ekojode <58553973+Ekojode@users.noreply.github.com>
2022-07-20 14:03:32 +0100

    Accessed Device Camera with Image_Picker Plugin

--- lib/widgets/image_input.dart
+++ lib/widgets/image_input.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class ImageInput extends StatefulWidget {
   const ImageInput({Key? key}) : super(key: key);
@@ -12,13 +13,21 @@ class ImageInput extends StatefulWidget {
 class _ImageInputState extends State<ImageInput> {
   File? _storedImage;
 
+  Future<void> _takePicture() async {
+    final ImagePicker picker = ImagePicker();
+    final XFile? imageFile = await picker.pickImage(
+      source: ImageSource.camera,
+      maxWidth: 600,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Row(
       children: [
         Container(
-          width: 100,
-          height: 100,
+          width: 150,
+          height: 150,
           decoration: BoxDecoration(
             border: Border.all(
               width: 1,
@@ -27,7 +36,10 @@ class _ImageInputState extends State<ImageInput> {
           ),
           alignment: Alignment.center,
           child: _storedImage == null
-              ? const Text("You have no image yet")
+              ? const Text(
+                  "You have no image yet",
+                  textAlign: TextAlign.center,
+                )
               : Image.file(
                   _storedImage!,
                   fit: BoxFit.cover,
@@ -37,7 +49,7 @@ class _ImageInputState extends State<ImageInput> {
         const SizedBox(width: 10),
         Expanded(
           child: TextButton.icon(
-            onPressed: () {},
+            onPressed: _takePicture,
             icon: const Icon(Icons.photo_camera),
             label: const Text(
               "Take Picture",
