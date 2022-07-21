1b44cd119518c3d0af9610528ab5f02b824f112a
Ekojode <58553973+Ekojode@users.noreply.github.com>
2022-07-21 14:15:08 +0100

    Stored image input to variable

--- lib/main.dart
+++ lib/main.dart
@@ -6,13 +6,14 @@ import '../screens/add_place_screen.dart';
 import '../providers/great_place_provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    const MyApp(),
+  );
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     final ThemeData theme = ThemeData(
--- lib/widgets/image_input.dart
+++ lib/widgets/image_input.dart
@@ -2,6 +2,8 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:path/path.dart' as path;
+import 'package:path_provider/path_provider.dart' as syspath;
 
 class ImageInput extends StatefulWidget {
   const ImageInput({Key? key}) : super(key: key);
@@ -19,6 +21,14 @@ class _ImageInputState extends State<ImageInput> {
       source: ImageSource.camera,
       maxWidth: 600,
     );
+
+    setState(() {
+      _storedImage = File(imageFile!.path);
+    });
+
+    final appDir = await syspath.getApplicationDocumentsDirectory();
+    final fileName = path.basename(imageFile!.path);
+    final savedImage = await imageFile.saveTo("${appDir.path} / $fileName");
   }
 
   @override
