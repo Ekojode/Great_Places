79c90d610fa384199d776d75ddb51b5f844eac1b
Ekojode <58553973+Ekojode@users.noreply.github.com>
2022-07-25 11:13:46 +0100

    Created access to device storage using sql

--- lib/helpers/db_helpers.dart
+++ lib/helpers/db_helpers.dart
@@ -4,7 +4,13 @@ import 'package:path/path.dart' as path;
 class DbHelper {
   static Future<void> insert(String table, Map<String, dynamic> data) async {
     final dbPath = await sql.getDatabasesPath();
-    sql.openDatabase(path.join(dbPath, "places.db"),
-        onCreate: ((db, version) {}), version: 1);
+    final sqlDb = await sql.openDatabase(path.join(dbPath, "places.db"),
+        onCreate: ((db, version) {
+      return db.execute(
+          "CREATE TABLE user_places (id TEXT PRIMARY KEY, title TEXT, image TEXT)");
+    }), version: 1);
+
+    await sqlDb.insert(table, data,
+        conflictAlgorithm: sql.ConflictAlgorithm.replace);
   }
 }
--- lib/providers/great_place_provider.dart
+++ lib/providers/great_place_provider.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 
 import '../models/places.dart';
+import '../helpers/db_helpers.dart';
 
 class GreatPlaces with ChangeNotifier {
   final List<Place> _items = [];
@@ -21,5 +22,13 @@ class GreatPlaces with ChangeNotifier {
 
     _items.add(newPlace);
     notifyListeners();
+    DbHelper.insert(
+      "places",
+      {
+        "id": newPlace.id,
+        "title": newPlace.name,
+        "image": newPlace.img!.path,
+      },
+    );
   }
 }
